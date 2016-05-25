ae0df4bf5949d6b22b9da0e6d1fc6e7d4389d80a
Eric Defore <eric.defore@gmail.com>
2016-05-25 10:25:58 -0400

    Fixed Gear Rotation in Header. Referencing #9

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,6 +4,7 @@
  @since 0.1.0
  @package RBMTheme
  */
+
 (function ($) {
     'use strict';
 
@@ -15,7 +16,9 @@
 
         if ($wrapper.hasClass('reveal')) {
             $wrapper.removeClass('reveal');
+            $logo.find( 'g.gear' ).addClass( 'reverse-spin' );
         } else {
+            $logo.find( 'g.gear' ).removeClass( 'reverse-spin' );
             $wrapper.addClass('reveal');
         }
     });
@@ -23,11 +26,22 @@
     // Hide when clicking on the back cover
     $('#back-cover').click(function () {
         $wrapper.removeClass('reveal');
+        $logo.find( 'g.gear' ).addClass( 'reverse-spin' );
     });
 
     // Hide when scrolling
+    var scrollInit = $( window ).scrollTop();
     $(window).scroll(function () {
-        $wrapper.removeClass('reveal');
+        
+        // Don't animate on Page Load
+        if ( $( window ).scrollTop() !== scrollInit ) {
+            scrollInit = 0; // Reset to 0 after any scrolling has happened. A page can be loaded with this value not being 0.
+            if ( $wrapper.hasClass( 'reveal' ) ) {
+                $wrapper.removeClass('reveal');
+                $logo.find( 'g.gear' ).addClass( 'reverse-spin' );
+            }
+        }
+        
     });
 
     // Only preview the header once per session
@@ -43,6 +57,7 @@
 
             setTimeout(function () {
                 $wrapper.removeClass('reveal');
+                $logo.find( 'g.gear' ).addClass( 'reverse-spin' );
                 $.cookie('rbm-header-preview', '1');
             }, 2000);
         }, 500 );
