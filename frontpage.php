0d3a4ec5bcdb3cf6361c95354300e0b487a3ddb9
Eric Defore <eric.defore@gmail.com>
2018-01-22 14:10:18 -0500

    This should not be a page template

---
+++ frontpage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Frontpage Template
+ *
+ * @package RBMTheme
+ */
+
+ get_header(); ?>
+
+ <?php get_template_part( 'template-parts/featured-image' ); ?>
+
+ <div class="main-wrap">
+	 <main class="main-content">
+		 <?php while ( have_posts() ) : the_post(); ?>
+		 	<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php comments_template(); ?>
+		 <?php endwhile;?>
+	 </main>
+ <?php get_sidebar(); ?>
+ </div>
+ <?php get_footer();
