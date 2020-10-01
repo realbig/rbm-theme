cbb3d9696545c2ceee0edcaf53799d5a1cae4b02
Eric Defore <eric.defore@gmail.com>
2020-10-01 13:37:35 -0400

    Add template for Jobs Single

---
+++ single-jobs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package RBMTheme
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+
+<div class="main-wrap full-width">
+	<main class="main-content">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/content', '' ); ?>
+			<?php the_post_navigation(); ?>
+		<?php endwhile;?>
+	</main>
+</div>
+<?php get_footer();
