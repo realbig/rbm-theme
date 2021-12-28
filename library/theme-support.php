bf6fc44b401fe52e5fb3da3bddcdb19799405e8a
Eric Defore <eric.defore@gmail.com>
2021-12-28 12:07:06 -0500

    Add theme styling to Editor

--- library/theme-support.php
+++ library/theme-support.php
@@ -41,6 +41,7 @@ function foundationpress_theme_support() {
     add_theme_support( 'wc-product-gallery-slider' );
 
 	// Add foundation.css as editor style https://codex.wordpress.org/Editor_Style
+	add_theme_support( 'editor-styles' );
 	add_editor_style( 'dist/assets/css/app.css' );
 }
 
